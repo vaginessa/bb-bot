5f6a57ddafbf8f66d48e5269474357a4fa67be7f
yashdsaraf <yashdsaraf@gmail.com>
2017-02-25 13:03:57 +0530

    Fix for importing python module (Trial 1)

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -18,6 +18,7 @@
 if [[ $TO_BUILD == "boxemup" ]]
 	then
 	sudo apt-get update -qq
+	sudo apt-get install -qq python3-pip
 	sudo pip3 install -qq requests
 else
 	sudo add-apt-repository ppa:jonathonf/automake -y
